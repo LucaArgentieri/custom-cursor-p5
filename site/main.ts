0b4ecda262ede8291db0d676115aecfe609fbbe1
LucaArgentieri <lucaargentieri-98@hotmail.it>
2022-08-03 17:14:36 +0200

    update gh-pages

--- site/main.ts
+++ site/main.ts
@@ -4,6 +4,7 @@ import { customCursor } from 'custom-cursor-p5'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <span custom-cursor-area style="pointer-events: none; position:fixed; left:0; top:0; width: 100vw; height:100vh; z-index: 9999;"></span>
 
+  
   <div style="display:flex; justify-content:center; align-items:center; height:100vh;">
     <h1>Custom Cursor!</h1>
   </div>
